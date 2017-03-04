4be227dbc434953893e45902274de86e4d01cc8a
patrycze <patryk.pazura@gmail.com>
2017-03-05 00:44:18 +0100

    add

--- sudokuSI/src/pazura/si/sudoku/Sudoku.java
+++ sudokuSI/src/pazura/si/sudoku/Sudoku.java
@@ -23,6 +23,7 @@ public class Sudoku {
 		String SudokuAsString = "003020600900305001001806400008102900700000008006708200002609500800203009005010300";
 		obj.FromString(SudokuAsString);
 		System.out.println(obj.toString());
+
 	}
 
 	@Override
